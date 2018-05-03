af62cb34eb1edbc3ec11ca7d9bf616a31237d271
jeffrothwell <rothwelljeff@gmail.com>
2018-05-03 15:32:00 -0400

    exclude unneeded api data, require dotenv gem to us API_KEY

--- server.rb
+++ server.rb
@@ -5,6 +5,6 @@ require 'dotenv/load'
 
 get '/:lat/:long.json' do
   content_type :json
-  response = HTTParty.get("https://api.darksky.net/forecast/#{ENV["REACT_APP_DARK_SKY_API_KEY"]}/#{params[:lat]},#{params[:long]}")
+  response = HTTParty.get("https://api.darksky.net/forecast/#{ENV["REACT_APP_DARK_SKY_API_KEY"]}/#{params[:lat]},#{params[:long]}?exclude=minutely,hourly,daily&units=ca")
   response.body
 end
