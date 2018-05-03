88b5c2b77b457531abf2f490b7cac69904a21fd0
jeffrothwell <rothwelljeff@gmail.com>
2018-05-03 12:29:41 -0400

    use key in env variable, change to .json url

--- server.rb
+++ server.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
+require 'httparty'
+require 'json'
+require 'dotenv/load'
 
-get '/' do
-  "Hello"
+get '/:lat/:long.json' do
+  content_type :json
+  response = HTTParty.get("https://api.darksky.net/forecast/#{ENV["REACT_APP_DARK_SKY_API_KEY"]}/#{params[:lat]},#{params[:long]}")
+  response.body
 end
